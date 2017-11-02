3d1ed785f2b83e10b0b2ab8182d5aa9f5391951a
Jeroenvdk <jeroenvdcage@gmail.com>
2017-11-02 11:51:38 +0100

    Variable changes when messsage is active.

--- js/app.js
+++ js/app.js
@@ -5,27 +5,32 @@ var msgInput = 0;
 detectMessage($); //detect that a message is there
 
 setNumber(msgInput);
-console.log(notNumber);
-chatBox();
+chatBox($);
 })(jQuery);
-
 function detectMessage($){
-	if($('.message').length > 0){
+	if($('#message').length > 0){
 		msgInput = 1;
 	}
 }
-function setNumber(msgInput){
+function setNumber(){
 	//check if the bot has send the message
     if (msgInput === 1) {
     	notNumber = 1;
     }
-    // make it a string to display it
-    notNumber.toString();
-
-    $('.notNumber').text(notNumber);
 }
-function chatBox(){
-	$(".popup-container").click(function() {
-	  $( ".chat-area" ).css("display", "none");
+function chatBox($){
+	var position = 'up';
+
+	$('.popup-button').click(function () {
+	      if (position == 'up') {
+	        $('.chat-area').css({'height' : '100%'});
+	        position = 'down';
+
+	        notNumber = 0;
+	      }
+	      else {
+	        $('.chat-area').css({'height' : '0%'});
+	        position = 'up';
+	      }
 	});
 }
